e920ec456c3f0d87538e19736c3ed2b51b50c307
kenmaz <kentaro.matsumae@gmail.com>
2020-07-05 01:08:30 +0900

    Update PathKit and Spectre for Xcode12 support

--- Package.swift
+++ Package.swift
@@ -11,10 +11,10 @@ let package = Package(
         .library(name: "ProjectSpec", targets: ["ProjectSpec"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
+        .package(url: "https://github.com/kenmaz/PathKit.git", from: "1.0.1"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
         .package(url: "https://github.com/yonaskolb/JSONUtilities.git", from: "4.2.0"),
-        .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0"),
+        .package(url: "https://github.com/kenmaz/Spectre.git", from: "0.10.0"),
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.0.0"),
         .package(url: "https://github.com/tuist/XcodeProj.git", .exact("7.10.0")),
         .package(url: "https://github.com/jakeheis/SwiftCLI.git", from: "6.0.0"),
